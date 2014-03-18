5caf2fce329837530dceea50e79089ca70213f17
MCarranzaLovesCookies <mike.carranza610@gmail.com>
2014-03-18 18:05:23 +0000

    We made a game called "Guess The Number!"

--- guess_the_number.rb
+++ guess_the_number.rb
@@ -1,3 +1,9 @@
 puts 'Welcome to "Guess the Number!"'
-secret_number = rand(65..70)
-puts secret_number
+secret_number = rand(999)
+puts "Guess a number between 0-999;"
+guess=gets.chomp.to_i
+if secret_number = guess
+  puts "Happy Birthday to You! "
+else
+  puts "Your Wrong, the number was #{secret_number} stupid! "
+end
